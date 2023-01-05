f3d2d76db29431a92be8d41b061ecf77bcb8d8d6
lonshanworld <lonshan3010@gmail.com>
2023-01-06 00:42:27 +0630

    11thtime_webpackconfigjs

--- p10_restaurantPage/webpack.config.js
+++ p10_restaurantPage/webpack.config.js
@@ -3,6 +3,7 @@ const path = require('path');
 module.exports = {
     mode: "development",
     entry: './src/index.js',
+    assetsPublicPath: '/p10_restaurantPage/',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
